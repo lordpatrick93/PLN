e9a863a628d0af56ded773ac76687176e50946bd
lordpatrick93 <69581169+lordpatrick93@users.noreply.github.com>
2020-09-16 00:13:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -133,7 +133,213 @@
                     	<td></td>
                     	<td>8. UPT BANDUNG</td>
                     </tr>
-                </table>
+			<hr class="tabel" align="center">
+<table class="body">
+<tr>
+<td>Level 3 :</td>
+	<td>1.ULP CIREBON KOTA</td>
+<td>Level 3 :</td>
+	<td>1.ULP BANDUNG UTARA</td>
+</tr>
+<tr>
+<td></td>
+	<td>2.ULP SUMBER</td>
+<td></td>
+	<td>2.ULP BANDUNG PRIMA</td>
+</tr>
+<tr>
+<td></td>
+	<td>3.ULP CILEDUG</td>
+<td></td>
+	</td>3.ULP BANDUNG SELATAN</td>
+</tr>
+<tr>
+<td></td>
+	<td>4.ULP CILIMUS</td>
+<td></td>
+	<td>4.ULP UJUNG BERUNG</td>
+</tr>
+<tr>
+<td></td>	
+	<td>5.ULP KUNINGAN</td>
+<td></td>
+	<td>5.ULP BANDUNG BARAT</td>
+</tr>
+<tr>
+<td></td>	
+	<td>6.ULP INDRAMAYU KOTA</td>
+<td></td>
+	<td>6.ULP BANDUNG TIMUR</td>
+</tr>
+<tr>
+<td></td>		
+	<td>7.ULP JATIBARANG</td>
+<td></td>
+	<td>7.ULP CIJAURA</td>
+</tr>
+<tr>
+<td></td>
+	<td>8.ULP HAURGEULIS</td>
+<td></td>
+	<td>8.ULP KOPO</td>
+</tr>
+<tr>
+<td></td>		
+	<td>9.ULP CIKEDUNG</td>
+<td></td>
+	<td>9.ULP TASIK KOTA</td>
+</tr>
+<tr>
+<td></td>		
+	<td>10.ULP SUMEDANG KOTA</td>
+<td></td>
+	<td>10.ULP RAJAPELAH</td>
+</tr>
+<tr>
+<td></td>		
+	<td>11.ULP MAJALENGKA</td>
+<td></td>
+	<td>11.ULP BANJAR</td>
+</tr>
+<tr>
+<td></td>	
+	<td>12.ULP TANJUNGSARI</td>
+<td></td>
+	<td>12.ULP PANGANDARAN</td>
+</tr>
+<tr>
+<td></td>	
+	<td>13.ULP JATIWANGI</td>
+<td></td>
+	<td>13.ULP SINGAPARNA</td>
+</tr>
+<tr>
+<td></td>		
+	<td>14.ULP PURWAKARTA KOTA</td>
+<td></td>
+	<td>14.ULP CIAMIS</td>
+</tr>
+<tr>
+<td></td>		
+	<td>15.ULP SUBANG</td>
+<td></td>
+	<td>15.ULP KARANG NUNGGAL</td>
+</tr>
+<tr>
+<td></td>		
+	<td>16.ULP PAMANUKAN</td>
+<td></td>
+	<td>16.ULP GARUT KOTA</td>
+</tr>
+<tr>
+<td></td>		
+	<td>17.ULP PLERED</td>
+<td></td>
+	<td>17.ULP CIKAJANG</td>
+</tr>
+<tr>
+<td></td>		
+	<td>18.ULP PAGADEN</td>
+<td></td>
+	<td>18.ULP CIBATU</td>
+</tr>
+<tr>
+<td></td>	
+	<td>19.ULP KARAWANG KOTA</td>
+<td></td>
+	<td>19.ULP LELES</td>
+</tr>
+<tr>
+<td></td>		
+	<td>20.ULP CIKAMPEK</td>
+<td></td>
+	<td>20.ULP PAMEMPEUK</td>
+</tr>
+<tr>
+<td></td>		
+	<td>21.ULP KOSAMBI</td>
+<td></td>
+	<td>21.ULP CIMAHI KOTA</td>
+</tr>
+<tr>
+<td></td>		
+	<td>22.ULP PRIMA KARAWANG</td>
+<td></td>
+	<td>22.ULP LEMBANG</td>
+</tr>
+<tr>
+<td></td>		
+	<td>23.ULP RENGASDENGKLOK</td>
+<td></td>
+	<td>23.ULP PADALARANG</td>
+</tr>
+<tr>
+<td></td>		
+	<td>24.BC HAR BEKASI</td>
+<td></td>
+	<td>24.ULP CILILIN</td>
+</tr>
+<tr>
+<td></td>		
+	<td>25.BC HAR CIREBON</td>
+<td></td>
+	<td>25.ULP RAJAMANDALA</td>
+</tr>
+<tr>
+<td></td>		
+	<td>26.BC HAR BANDUNG</td>
+<td></td>
+	<td>26.ULP PRIMA CIBABAT</td>	
+</tr>	
+<td>27.BC HAR TASIKMALAYA</td>
+<td>28.BC HAR KARAWANG</td>
+<td>29.BC HAR BOGOR</td>
+<td>30.ULTG CIREBON</td>
+<td>31.ULTG GARUT</td>
+<td>32.GI CIAMIS</td>
+<td>33.GI CIKEDUNG</td>
+<td>34.GI DARAJAT</td>
+<td>35.GI HAURGEULIS</td>
+<td>36.GI INDRAMAYU</td>
+<td>37.GI JATIBARANG</td>
+<td>38.GI KADIPATEN</td>
+<td>39.GI KAMOJANG</td>
+<td>40.GI KARANGNUNGGAL</td>
+<td>41.GI KUNINGAN</td>
+<td>42.GI MALANGBONG</td>
+<td>43.GI PAMENPEUK</td>
+<td>44.GI PANGANDARAN</td>
+<td>45.GI PARAKAN</td>
+<td>46.GI SUMADRA</td>
+<td>47.GI TASIKMALAYA</td>
+<td>48.GITET TASIKMALAYA</td>
+<td>49.ULTG PURWAKARTA</td>
+<td>50.ULTG KARAWANG</td>
+<td>51.GI CIKARANG</td>
+<td>52.GI CIKUPAY</td>
+<td>53.GI CIRATA</td>
+<td>54.GI DAWUAN</td>
+<td>55.GI FAJAR</td>
+<td>56.GI GANDAMEKAR</td>
+<td>57.GI HANKOOK</td>
+<td>58.GI INDORAMA</td>
+<td>59.GI JABABEKA</td>
+<td>60.GI JUISHIN</td>
+<td>61.GI MEKARSARI</td>
+<td>62.GI PABUARAN</td>
+<td>64.GI PARUNGMULYA</td>
+<td>65.GI PURWAKARTA</td>
+<td>66.GI RENGASDENGKLOK</td>
+<td>67.GI SUBANG</td>
+<td>68.GI SUKAMANDI</td>
+<td>69.GI TAMBUN</td>
+<td>70.GI TEGAL HERANG</td>
+<td>71.GI TELUK JAMBE</td>
+<td>72.GITET MUARA TAWAR</td>
+</tr>
+
+
+                </table>  
 
             </div>
         </form>
